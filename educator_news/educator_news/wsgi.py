319c96815dc7eaadca1c6c0e23a11301fc9d8079
Eric Matthes <ehmatthes@gmail.com>
2014-01-14 06:54:05 -0900

    Initial commit.

---
+++ educator_news/educator_news/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for educator_news project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educator_news.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
