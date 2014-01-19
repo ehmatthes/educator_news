47a7ddbca7da2bf189efc954c6b451c4b97c7e2d
Eric Matthes <ehmatthes@gmail.com>
2014-01-18 23:39:01 -0900

    Login and logout functionality works.

--- educator_news/ed_news/views.py
+++ educator_news/ed_news/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
+from django.contrib.auth import logout
+from django.core.urlresolvers import reverse
 
 def index(request):
     # Static index page for now.
     return render_to_response('ed_news/index.html',
                               {},
                               context_instance = RequestContext(request))
+
+
+# Authentication views
+def logout_view(request):
+    logout(request)
+    # Redirect to home page.
+    return redirect('/')
--- educator_news/educator_news/urls.py
+++ educator_news/educator_news/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.core.urlresolvers import reverse
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,4 +9,8 @@ urlpatterns = patterns('',
     url(r'^', include('ed_news.urls', namespace='ed_news')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    # Auth urls
+    url(r'^login/', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout/', 'ed_news.views.logout_view', name='logout_view'),
 )
