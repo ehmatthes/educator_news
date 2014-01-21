b01bcd493f450efbb3e3a83f593017a2b2dd3009
Eric Matthes <ehmatthes@gmail.com>
2014-01-20 23:30:10 -0900

    Simple registration with User model, no UserProfile, works.

--- educator_news/ed_news/models.py
+++ educator_news/ed_news/models.py
@@ -4,4 +4,11 @@ from django.contrib.auth.models import User
 # --- Educator News models ---
 
 class UserProfile(models.Model):
+    # Link UserProfile to a User instance.
     user = models.OneToOneField(User)
+
+    # Custom user fields, not in User model.
+
+    # Use username to refer to user.
+    def __unicode__(self):
+        return self.user.username
--- educator_news/ed_news/views.py
+++ educator_news/ed_news/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
 from django.core.urlresolvers import reverse
 from django.contrib.auth.views import password_change
+from ed_news.forms import UserForm, UserProfileForm
 
 def index(request):
     # Static index page for now.
@@ -35,3 +36,41 @@ def password_change_successful(request):
     return render_to_response('registration/password_change_successful.html',
                               {},
                               context_instance = RequestContext(request))
+
+def register(request):
+    # Assume registration won't work.
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+
+        if user_form.is_valid():
+            # Save user's form data.
+            user = user_form.save()
+            print 'un', user.username
+
+            user.set_password(user.password)
+            user.save()
+
+            # Registration was successful.
+            registered = True
+
+            # Not requiring email validation yet, so log user in.
+            #user = authenticate(username=user.username, password=user.password)
+            #login(request, user)
+            print 'un, pw', user.username,  user.password
+
+        else:
+            # Invalid form/s.
+            print 'ufe', user_form.errors
+            print 'uf', user_form
+
+    else:
+        # Send blank forms.
+        user_form = UserForm()
+
+    return render_to_response('registration/register.html',
+                                  {'user_form': user_form,
+                                   'registered': registered,
+                                   },
+                                  context_instance = RequestContext(request))
