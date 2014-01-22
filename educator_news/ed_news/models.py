41f458279f56c6adcde9383b89b133c391500332
Eric Matthes <ehmatthes@gmail.com>
2014-01-22 06:55:35 -0900

    Started Submission, Article models.

--- educator_news/ed_news/models.py
+++ educator_news/ed_news/models.py
@@ -4,4 +4,97 @@ from django.contrib.auth.models import User
 # --- Educator News models ---
 
 class UserProfile(models.Model):
+    # Link UserProfile to a User instance.
     user = models.OneToOneField(User)
+
+    # Custom user fields, not in User model.
+
+    # Use username to refer to user.
+    def __unicode__(self):
+        return self.user.username
+
+class Submission(models.Model):
+    """ A parent class for the two types of submission,
+    which are an article and a text submission. Text submissions
+    can be questions, ie Ask EN, or posts such as Show EN.
+
+    Using multi-table inheritance rather than abstract base class,
+    because I want to be able to query back to a general submission,
+    and then deal with the specific type.
+    """
+
+    ARTICLE = 'ART'
+    TEXT = 'TXT'
+    SUBMISSION_TYPE_CHOICES = (
+        (ARTICLE, 'Article'),
+        (TEXT, 'Text'),
+        )
+
+    # Stick with programming 80-char limit for now.
+    #  It's what HN uses, which fits nicely on mobile.
+    title = models.CharField(max_length=80)
+    author = models.OneToOneField(UserProfile)
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    points = models.IntegerField(default=0)
+    submission_time = models.DateTimeField(auto_now_add=True)
+    submission_type = models.CharField(max_length=3,
+                                       choices=SUBMISSION_TYPE_CHOICES,
+                                       default=ARTICLE)
+
+class Article(Submission):
+    url = URLField()
+
+    Using multi-table inheritance rather than abstract base class,
+    because I want to be able to query back to a general submission,
+    and then deal with the specific type.
+    """
+
+    ARTICLE = 'ART'
+    TEXT = 'TXT'
+    SUBMISSION_TYPE_CHOICES = (
+        (ARTICLE, 'Article'),
+        (TEXT, 'Text'),
+        )
+
+    # Stick with programming 80-char limit for now.
+    #  It's what HN uses, which fits nicely on mobile.
+    title = models.CharField(max_length=80)
+    author = models.OneToOneField(UserProfile)
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    points = models.IntegerField(default=0)
+    submission_time = models.DateTimeField(auto_now_add=True)
+    submission_type = models.CharField(max_length=3,
+                                       choices=SUBMISSION_TYPE_CHOICES,
+                                       default=ARTICLE)
+
+class Article(Submission):
+    url = URLField()
+
+    Using multi-table inheritance rather than abstract base class,
+    because I want to be able to query back to a general submission,
+    and then deal with the specific type.
+    """
+
+    ARTICLE = 'ART'
+    TEXT = 'TXT'
+    SUBMISSION_TYPE_CHOICES = (
+        (ARTICLE, 'Article'),
+        (TEXT, 'Text'),
+        )
+
+    # Stick with programming 80-char limit for now.
+    #  It's what HN uses, which fits nicely on mobile.
+    title = models.CharField(max_length=80)
+    author = models.OneToOneField(UserProfile)
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    points = models.IntegerField(default=0)
+    submission_time = models.DateTimeField(auto_now_add=True)
+    submission_type = models.CharField(max_length=3,
+                                       choices=SUBMISSION_TYPE_CHOICES,
+                                       default=ARTICLE)
+
+class Article(Submission):
+    url = URLField()
