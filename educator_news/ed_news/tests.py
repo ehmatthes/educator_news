ec4e32529a046061b48f046fa9b26a88fc1d6453
Eric Matthes <ehmatthes@gmail.com>
2014-02-24 06:45:07 -0900

    Tests that decrementing karma removes user from active_members.

--- educator_news/ed_news/tests.py
+++ educator_news/ed_news/tests.py
@@ -6,7 +6,7 @@ from ed_news.views import invalidate_cache
 from django.contrib.auth.models import User, Group
 from ed_news.models import UserProfile
 from ed_news.views import KARMA_LEVEL_ACTIVE_MEMBERS
-from ed_news.views import increment_karma, is_active_member
+from ed_news.views import increment_karma, decrement_karma, is_active_member
 
 class EdNewsViewTests(TestCase):
 
@@ -45,18 +45,30 @@ class EdNewsViewTests(TestCase):
         If a member crosses the active_member threshold,
         they should be put in the active_member group automatically.
         """
+        # Create a new user and userprofile.
         new_user = User(username='paulo', password='password')
         new_user.save()
         new_user_profile = UserProfile(user=new_user)
         new_user_profile.save()
 
-        # Need to make the group first?
+        # Make active_members group.
+        #  This should run the make_groups.py script.
         active_members = Group(name='Active Members')
         active_members.save()
 
+        # Set karma just below critical level.
+        #  Test that user is not in active_members.
         new_user_profile.karma = KARMA_LEVEL_ACTIVE_MEMBERS - 1
         self.assertEqual(is_active_member(new_user), False)
 
+        # Increment karma past threshold, not just to threshold.
+        #  Test that user is now in active_members.
         increment_karma(new_user)
         increment_karma(new_user)
         self.assertEqual(is_active_member(new_user), True)
+
+        # Decrement karma below threshold.
+        #  Test that user is no longer in active_members.
+        decrement_karma(new_user)
+        decrement_karma(new_user)
+        self.assertEqual(is_active_member(new_user), False)
