5b82f7a706ffd2c0bfd90f4c9c660e73b5d9cabd
Eric Matthes <ehmatthes@gmail.com>
2014-03-05 06:55:51 -0900

    Different parameters for running tests.

--- educator_news/ed_news/tests.py
+++ educator_news/ed_news/tests.py
@@ -115,19 +115,35 @@ class EdNewsViewTests(TestCase):
         # Create a number of comments on each submission.
         # Create a random number of upvotes and downvotes.
 
-        num_users = 2
-        # Number of links each user submits.
-        num_link_submissions = 2
-        # Number of text posts each user submits.
-        num_textpost_submissions = 2
-        # Number of submissions each user comments on.
-        num_comments = 3
-        # Number of comments each user replies to.
-        num_replies = 2
-        # Number of items each user will vote/ flag.
-        num_submission_upvotes = 2
-        num_comment_upvotes = 2
-        num_comment_downvotes = 1
+        size = 'medium'
+        if size == 'tiny':
+            num_users = 2
+            # Number of links each user submits.
+            num_link_submissions = 2
+            # Number of text posts each user submits.
+            num_textpost_submissions = 2
+            # Number of submissions each user comments on.
+            num_comments = 3
+            # Number of comments each user replies to.
+            num_replies = 2
+            # Number of items each user will vote/ flag.
+            num_submission_upvotes = 2
+            num_comment_upvotes = 2
+            num_comment_downvotes = 1
+        elif size == 'medium':
+            num_users = 50
+            # Number of links each user submits.
+            num_link_submissions = 3
+            # Number of text posts each user submits.
+            num_textpost_submissions = 2
+            # Number of submissions each user comments on.
+            num_comments = 5
+            # Number of comments each user replies to.
+            num_replies = 3
+            # Number of items each user will vote/ flag.
+            num_submission_upvotes = 10
+            num_comment_upvotes = 10
+            num_comment_downvotes = 1
 
         for x in range(0,num_users):
             # Each user's password is their username.
