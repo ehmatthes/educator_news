555542085c9b60cc096cbb68a454778c41dd193e
Eric Matthes <ehmatthes@gmail.com>
2014-03-11 22:51:03 -0800

    Attempt at caching an individual queryset.

--- educator_news/ed_news/views.py
+++ educator_news/ed_news/views.py
@@ -32,14 +32,20 @@ FLAGS_TO_DISAPPEAR = 1
 # How long does a user have to edit a comment?
 COMMENT_EDIT_WINDOW = 60*10
 
-@cache_page(60 * 1)
+#@cache_page(60 * 1)
 def index(request):
     # Get a list of submissions, sorted by date.
-
+    #  Try to get cached version if available.
     if request.user.is_authenticated() and request.user.userprofile.show_invisible:
-        submissions = Submission.objects.all().order_by('ranking_points', 'submission_time').reverse()[:MAX_SUBMISSIONS].prefetch_related('flags', 'upvotes', 'comment_set')
+        submissions = cache.get('index_cache_show_invisible')
+        if not submissions:
+            submissions = Submission.objects.all().order_by('ranking_points', 'submission_time').reverse()[:MAX_SUBMISSIONS].prefetch_related('flags', 'upvotes', 'comment_set')
+            cache.set('index_cache_show_invisible', submissions)
     else:
-        submissions = Submission.objects.filter(visible=True).order_by('ranking_points', 'submission_time').reverse()[:MAX_SUBMISSIONS].prefetch_related('flags', 'upvotes', 'comment_set')
+        submissions = cache.get('index_cache')
+        if not submissions:
+            submissions = Submission.objects.filter(visible=True).order_by('ranking_points', 'submission_time').reverse()[:MAX_SUBMISSIONS].prefetch_related('flags', 'upvotes', 'comment_set', 'submitter')
+            cache.set('index_cache', submissions)
         
     submission_set = get_submission_set(submissions, request.user)
 
