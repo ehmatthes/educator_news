d464ca5b8551510886ae01d57120baf75f2fe53a
Eric Matthes <ehmatthes@gmail.com>
2014-03-24 06:05:03 -0800

    Commented that query.

--- educator_news/ed_news/views.py
+++ educator_news/ed_news/views.py
@@ -1059,6 +1059,7 @@ def get_comment_set_conversations(request, comment_set):
     #  Why doesn't prefetch_related('comment_set') grab replies from other authors?
     #  These are not all first-order comments; some are in the same thread.
     #user_comments = Comment.objects.filter(author=request.user).order_by('submission_time').reverse().prefetch_related('upvotes', 'downvotes', 'flags', 'comment_set', 'author', 'parent_comment')
+    # This is slightly more efficient.
     user_comments = request.user.comments.order_by('submission_time').reverse().prefetch_related('upvotes', 'downvotes', 'flags', 'comment_set', 'author', 'parent_comment')
 
     # Need to get first order comments.
